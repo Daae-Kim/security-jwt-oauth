65a856807f65952303b1210a5b0d1270e333711f
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-07-14 00:45:23 +0900

    feat : 기본적인 security config 설정

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+//security config 설정
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable 왜 session 에서는 필수이고, 토큰에서는 하지 않을까? stateless 와 관계성 정리 필요
+        http
+                .csrf((auth) -> auth.disable());
+
+        //form 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+        //http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+        //세션 stateless 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+}
