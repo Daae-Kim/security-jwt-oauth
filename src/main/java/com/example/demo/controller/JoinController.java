8095df0b273bcb67db4f6715dc7259c5a2019301
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-07-14 00:44:40 +0900

    feat : join controller 생성

---
+++ src/main/java/com/example/demo/controller/JoinController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.JoinDTO;
+import com.example.demo.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+        joinService.joinProcess(joinDTO);
+        return "ok(회원가입 완료)";
+    }
+}
