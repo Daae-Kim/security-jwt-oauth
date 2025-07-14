d17f19a58c26c4bf0cc66d66959237c949f38cca
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-07-15 01:18:02 +0900

    feat : contextHolder 권한, 사용자 이름 조회

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,6 +1,11 @@
 package com.example.demo.controller;
 
 
+import java.util.Collection;
+import java.util.Iterator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +16,18 @@ public class MainController {
 
     @GetMapping("/")
     public String mainP() {
-        return "Main Controller";
+        //세션 현재 사용자 아이디 : SecurityContextHolder.getContext().getAuthentication().getname();
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+
+        return "Main Controller"+ name + role;
     }
 
 }
