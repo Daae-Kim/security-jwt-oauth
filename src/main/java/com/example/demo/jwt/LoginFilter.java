69613580a2c913b89e3646260f5d0a730b6e21c5
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-07-14 19:20:13 +0900

    feat : login 성공시 유저 정보로 jwt 토큰 생성

--- src/main/java/com/example/demo/jwt/LoginFilter.java
+++ src/main/java/com/example/demo/jwt/LoginFilter.java
@@ -1,20 +1,27 @@
 package com.example.demo.jwt;
 
+import com.example.demo.dto.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import java.util.Iterator;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
 
-    public LoginFilter(AuthenticationManager authenticationManager) {
+    private final JWTUtil jwtUtil;
+
+    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
 
     //loginform 을 비활성화하며 usernamePasswordAuthenticationFilter 도 함께 비활성화 되어 직접 커스텀, 사용
@@ -37,12 +44,27 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,Authentication authentication){
+        //성공시 jwt발급
+        //정보 뽑아내기
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal(); // 특정 유저 확인
+        String username = userDetails.getUsername();
 
-        System.out.println("successful authentication");
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority grantedAuthority = iterator.next();
+
+        String role = grantedAuthority.getAuthority();
+
+        //정보로 jwt 토큰 생성
+        String token = jwtUtil.createJwt(username, role, 60*60*10L);
+
+        //http 인증 방식은 RFC 7235 정의에 따라 인증헤더 형태를 가져야 한다.
+        response.addHeader("Authorization", "Bearer " + token);
+
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed){
-        System.out.println("fail authentication");
+        response.setStatus(401);
     }
 }
